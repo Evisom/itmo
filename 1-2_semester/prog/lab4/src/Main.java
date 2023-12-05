6492b6b3b30fd433c8f50ca92cf337d469747767
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2023-12-05 18:46:59 +0300

    prog rename

--- 1-2_semester/prog/lab4/src/Main.java
+++
@@ -1,9 +0,0 @@
-import entities.Human;
-
-public class Main {
-    public static void main(String[] args) {
-        Human human = new Human("Alice", 17, 175, 58);
-        human.info();
-//        System.out.println("Hello world!");
-    }
-}
---
+++ 1-2_semester/programming/lab567/server/src/main/server/Server.java
@@ -0,0 +1,7 @@
+package server;
+
+class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello, server!");
+    }
+}
